5eb2bd52b95ce52107819854fb9aa436ca6b9fd7
locstein <raffy16p@gmail.com>
2023-09-14 22:32:50 +0800

    adjusted the space from sidebar to main content

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,7 +2,7 @@ import Sidebar from './Sidebar'
 
 const RootLayout = ({children}) => {
   return (
-    <div className='flex gap-5'>
+    <div className='flex'>
       <Sidebar />
       <main className="max-w-5xl flex-1 mx-auto py-4">{children}</main>
     </div>
